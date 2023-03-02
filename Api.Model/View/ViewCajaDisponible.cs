1e3cbb6533f82b09a8d2afb044fe61c566eddd8d
Oscar <you@example.com>
2023-03-02 06:56:12 -0600

    first commit

---
+++ Api.Model/View/ViewCajaDisponible.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Model.View
+{
+    public class ViewCajaDisponible
+    {
+        public string Caja { get; set; }
+        public string Ubicacion { get; set; }
+        public string Sucursal { get; set; }
+    }
+}
